bbc55d54cc7e0e74280cb32b26488417a73e9e09
GuiAlvezzzzzz <guilherme.santos11@eaportal.org>
2025-01-08 19:04:14 -0300

    Responsividade Para Celular

--- src/App.js
+++ src/App.js
@@ -47,8 +47,84 @@ function App() {
   ];
   
   const handleScroll = () => {
-    const scrollPosition = window.scrollY;
+  const scrollPosition = window.scrollY;
+  const isMobile = window.innerWidth <= 768;  // Considera como mobile dispositivos com largura <= 768px
 
+  if (isMobile) {
+    // Ajuste para a versão mobile, se necessário
+    if (scrollPosition < 500) {
+      setBackgroundColor('white'); // "College Dropout"
+      seth2Color('gold'); // Cor do texto para "College Dropout"
+      setpColor('black');
+      setMusicaAtual(musicas[0].musica);
+    } else if (scrollPosition >= 500 && scrollPosition < 1300) {
+      setBackgroundColor('black'); // "Late Registration"
+      seth2Color('white'); // Cor do texto para "Late Registration"
+      setpColor('white');
+      setMusicaAtual(musicas[1].musica);
+    } else if (scrollPosition >= 1300 && scrollPosition < 2000) {
+      setBackgroundColor('purple'); // "Graduation"
+      seth2Color('pink'); // Cor do texto para "Graduation"
+      setpColor('pink');
+      setMusicaAtual(musicas[2].musica);
+    } else if (scrollPosition >= 2000 && scrollPosition < 2700) {
+      setBackgroundColor('grey'); // "808"
+      seth2Color('black'); // Cor do texto para "808"
+      setpColor('black');
+      setMusicaAtual(musicas[3].musica);
+    } else if (scrollPosition >= 2700 && scrollPosition < 3500) {
+      setBackgroundColor('red'); // "MBDTF"
+      seth2Color('white'); // Cor do texto para "MBDTF"
+      setpColor('white');
+      setMusicaAtual(musicas[4].musica);
+    } else if (scrollPosition >= 3500 && scrollPosition < 4400) {
+      setBackgroundColor('#CD7F32'); // "WATCH"
+      seth2Color('white'); // Cor do texto para "WATCH"
+      setpColor('white');
+      setMusicaAtual(musicas[5].musica);
+    } else if (scrollPosition >= 4400 && scrollPosition < 5200) {
+      setBackgroundColor('#FFF'); // "YEZZUS"
+      seth2Color('red'); // Cor do texto para "YEZZUS"
+      setpColor('red');
+      setMusicaAtual(musicas[6].musica);
+    } else if (scrollPosition >= 5200 && scrollPosition < 5800){
+      setBackgroundColor('#FF7F50'); // "PABLO"
+      seth2Color('black'); // Cor do texto para "PABLO"
+      setpColor('black');
+      setMusicaAtual(musicas[7].musica);
+    } else if (scrollPosition >= 5800 &&scrollPosition < 6500){
+      setBackgroundColor('#020D3F'); // "ye"
+      seth2Color('#50CF01'); // Cor do texto para "ye"
+      setpColor('#50CF01');
+      setMusicaAtual(musicas[8].musica);
+    } else if (scrollPosition >= 6500 && scrollPosition < 7000){
+      setBackgroundColor('yellow'); // "kids"
+      seth2Color('black'); // Cor do texto para "kids"
+      setpColor('black');
+      setMusicaAtual(musicas[9].musica);
+    } else if (scrollPosition >= 7000 && scrollPosition < 7500) {
+      setBackgroundColor('white'); // "JIK"
+      seth2Color('blue'); // Cor do texto para "JIK"
+      setpColor('blue');
+      setMusicaAtual(musicas[10].musica);
+    } else if (scrollPosition >= 7500 && scrollPosition < 8000) {
+      setBackgroundColor('Black'); // "DONDA"
+      seth2Color('white'); // Cor do texto para "DONDA"
+      setpColor('white');
+      setMusicaAtual(musicas[11].musica);
+    } else if (scrollPosition >= 8000 && scrollPosition <8500) {
+      setBackgroundColor('#d4d1be'); // "VULTURES 1"
+      seth2Color('black'); // Cor do texto para "VULTURES 1"
+      setpColor('black');
+      setMusicaAtual(musicas[12].musica);
+    } else if (scrollPosition >= 8500 && scrollPosition < 9000) {
+      setBackgroundColor('#CEC6B3'); // "VULTURES 2"
+      seth2Color('black'); // Cor do texto para "VULTURES 2"
+      setpColor('black');
+      setMusicaAtual(musicas[13].musica);
+    }
+  } else {
+    // Ajuste para telas maiores
     if (scrollPosition < 600) {
       setBackgroundColor('white'); // "College Dropout"
       seth2Color('gold'); // Cor do texto para "College Dropout"
@@ -74,55 +150,55 @@ function App() {
       seth2Color('white'); // Cor do texto para "MBDTF"
       setpColor('white');
       setMusicaAtual(musicas[4].musica);
-    } else if (scrollPosition >= 4400 && scrollPosition < 5500)        
-      {setBackgroundColor('#CD7F32'); // "WATCH"
+    } else if (scrollPosition >= 4400 && scrollPosition < 5500) {
+      setBackgroundColor('#CD7F32'); // "WATCH"
       seth2Color('white'); // Cor do texto para "WATCH"
       setpColor('white');
       setMusicaAtual(musicas[5].musica);
-    } else if (scrollPosition >= 5500 && scrollPosition < 6500) 
-      {setBackgroundColor('#FFF'); // "YEZZUS"
+    } else if (scrollPosition >= 5500 && scrollPosition < 6500) {
+      setBackgroundColor('#FFF'); // "YEZZUS"
       seth2Color('red'); // Cor do texto para "YEZZUS"
       setpColor('red');
       setMusicaAtual(musicas[6].musica);
-    } else if (scrollPosition >= 6500 && scrollPosition < 7600) 
-      {setBackgroundColor('#FF7F50'); // "PABLO"
+    } else if (scrollPosition >= 6500 && scrollPosition < 7600) {
+      setBackgroundColor('#FF7F50'); // "PABLO"
       seth2Color('black'); // Cor do texto para "PABLO"
       setpColor('black');
       setMusicaAtual(musicas[7].musica);
-    } else if (scrollPosition >= 7600 && scrollPosition < 8600) 
-      {setBackgroundColor('#020D3F'); // "ye"
+    } else if (scrollPosition >= 7600 && scrollPosition < 8600) {
+      setBackgroundColor('#020D3F'); // "ye"
       seth2Color('#50CF01'); // Cor do texto para "ye"
       setpColor('#50CF01');
       setMusicaAtual(musicas[8].musica);
-    } else if (scrollPosition >= 8600 && scrollPosition < 9600) 
-      {setBackgroundColor('yellow'); // "kids"
+    } else if (scrollPosition >= 8600 && scrollPosition < 9600) {
+      setBackgroundColor('yellow'); // "kids"
       seth2Color('black'); // Cor do texto para "kids"
       setpColor('black');
       setMusicaAtual(musicas[9].musica);
-    } else if (scrollPosition >= 9601 && scrollPosition < 10600) 
-      {setBackgroundColor('white'); // "JIK"
+    } else if (scrollPosition >= 9601 && scrollPosition < 10600) {
+      setBackgroundColor('white'); // "JIK"
       seth2Color('blue'); // Cor do texto para "JIK"
       setpColor('blue');
       setMusicaAtual(musicas[10].musica);
-    } else if (scrollPosition >= 10600 && scrollPosition < 11600) 
-      {setBackgroundColor('Black'); // "DONDA"
+    } else if (scrollPosition >= 10600 && scrollPosition < 11600) {
+      setBackgroundColor('Black'); // "DONDA"
       seth2Color('white'); // Cor do texto para "DONDA"
       setpColor('white');
       setMusicaAtual(musicas[11].musica);
-    } else if (scrollPosition >= 11600 && scrollPosition < 12600) 
-      {setBackgroundColor('#d4d1be'); // "VULTURES 1"
+    } else if (scrollPosition >= 11600 && scrollPosition < 12600) {
+      setBackgroundColor('#d4d1be'); // "VULTURES 1"
       seth2Color('black'); // Cor do texto para "VULTURES 1"
       setpColor('black');
       setMusicaAtual(musicas[12].musica);
-    } else if (scrollPosition >= 12600 && scrollPosition < 13600) 
-      {setBackgroundColor('#CEC6B3'); // "VULTURES 2"
+    } else if (scrollPosition >= 12600 && scrollPosition < 13600) {
+      setBackgroundColor('#CEC6B3'); // "VULTURES 2"
       seth2Color('black'); // Cor do texto para "VULTURES 2"
       setpColor('black');
       setMusicaAtual(musicas[13].musica);
     }
-  };
+  }
+};
 
-  // Adiciona o ouvinte de evento de scroll quando o componente for montado
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
 
@@ -144,7 +220,6 @@ function App() {
 
 
 
-  // Inicialização do AOS (Animações ao rolar)
   useEffect(() => {
     AOS.init({
       duration: 2000, 
@@ -167,7 +242,7 @@ function App() {
       observer.observe(section);
     });
 
-    // Limpeza
+
     return () => {
       observer.disconnect();
     };
@@ -181,7 +256,6 @@ function App() {
   return (
     <div className="container" style={{ backgroundColor: backgroundColor }}>   <audio key={musicaAtual} autoPlay loop>
           <source src={musicaAtual} type="audio/mp3" />
-          Seu navegador não suporta o elemento de áudio.
         </audio>
       <header className="header">
         <h1 className="title" style={{ color: h2Color }}>Discografia de Kanye West</h1>
@@ -197,6 +271,7 @@ function App() {
           </button>
         </div>
       </header>
+
 
       {/* Seção The College Dropout */}
       <section id="college-dropout" className="section">
@@ -220,7 +295,7 @@ function App() {
         <p style={{ color: pColor }}>
           "Late Registration" é o segundo álbum de Kanye West, lançado em 2005. O álbum apresenta músicas como "Gold Digger", "Diamonds from Sierra Leone" e "Touch the Sky".
         </p>
-        <a href="https://open.spotify.com/album/5eJfsD2dbw9wY5bYq5bD4r">
+        <a href="https://open.spotify.com/intl-pt/album/5ll74bqtkcXlKE7wwkMq4g?si=unFckB9uTmGB6L7wBEJ41Q">
           <img className="platform-icon" src="/spotify.png" alt="Spotify" />
         </a>
         <a href="https://music.apple.com/us/album/late-registration/1440786557">
@@ -235,7 +310,7 @@ function App() {
         <p style={{ color: pColor }}>
           "Graduation" é o terceiro álbum de Kanye West, lançado em 2007. O álbum inclui faixas como "Stronger", "Good Life" e "Can't Tell Me Nothing".
         </p>
-        <a href="https://open.spotify.com/album/7zsi1qDCT0DCqWnq0uMGWa">
+        <a href="https://open.spotify.com/intl-pt/album/4SZko61aMnmgvNhfhgTuD3?si=yRVSPGA2R7Cxj29vP4jp_g">
           <img className="platform-icon" src="/spotify.png" alt="Spotify" />
         </a>
         <a href="https://music.apple.com/us/album/graduation/1440835365">
@@ -251,7 +326,7 @@ function App() {
         <p style={{ color: pColor }}>
           "808s and heartbreak" é o quarto álbum de Kanye West, lançado em 2008. O álbum inclui faixas como "Heartless", "Love Lockdown" e "Paranoid".
         </p>
-        <a href="https://open.spotify.com/album/7zsi1qDCT0DCqWnq0uMGWa">
+        <a href="https://open.spotify.com/intl-pt/album/3WFTGIO6E3Xh4paEOBY9OU?si=m48M9iroTuyLJ4Vlrws6IA">
           <img className="platform-icon" src="/spotify.png" alt="Spotify" />
         </a>
         <a href="https://music.apple.com/us/album/graduation/1440835365">
@@ -294,7 +369,7 @@ function App() {
         <p>
           Curiosidade: O Álbum Contém um total de 7 Capas alternativas, representando uma música em especifico.
         </p>
-        <a href="https://open.spotify.com/intl-pt/album/20r762YmB5HeofjMCiPMLv?si=RHC2H_vOQxOoB1JlMaqwyg">
+        <a href="https://open.spotify.com/intl-pt/album/20r762YmB5HeofjMCiPMLv?si=qc8LK2E6TU2u1B_lm8JLBA">
           <img className="platform-icon" src="/spotify.png" alt="Spotify" />
         </a>
         <a href="https://music.apple.com/us/album/graduation/1440835365">
@@ -308,7 +383,7 @@ function App() {
         <p style={{ color: pColor }}>
           "Watch The Throne" é o sexto álbum de Kanye West, lançado em colaboração com o Rapper JAY-Z,lançado em 2011. O álbum inclui faixas como "No Church In The Wild", "Ni**as In Paris" e "Why I Love You".
         </p>
-        <a href="https://open.spotify.com/intl-pt/album/0OcMap99vLEeGkBCfCwRwS?si=OTZyWGw8T_21_4LABs_d0A">
+        <a href="https://open.spotify.com/intl-pt/album/0OcMap99vLEeGkBCfCwRwS?si=thS51IJgTNqOE8IPTqBL_Q">
           <img className="platform-icon" src="/spotify.png" alt="Spotify" />
         </a>
         <a href="https://music.apple.com/us/album/graduation/1440835365">
@@ -322,7 +397,7 @@ function App() {
         <p style={{ color: pColor }}>
           "Yeezus" é o sétimo álbum de Kanye West, lançado em 2013. O álbum inclui faixas como "On Sight", "Black Skinhead" e "Bound 2".
         </p>
-        <a href="https://open.spotify.com/intl-pt/album/7D2NdGvBHIavgLhmcwhluK?si=N-1yvl95R76Qe1a8BcddMQ">
+        <a href="https://open.spotify.com/intl-pt/album/7D2NdGvBHIavgLhmcwhluK?si=t6SQMB4pTv2cp3DmJ0gvLQ">
           <img className="platform-icon" src="/spotify.png" alt="Spotify" />
         </a>
         <a href="https://music.apple.com/us/album/graduation/1440835365">
@@ -336,7 +411,7 @@ function App() {
         <p style={{ color: pColor }}>
           "The Life of Pablo" é o oitavo álbum de Kanye West, lançado em 2016. O álbum inclui faixas como "Ultralight Beam", "Father Sretch My Hands Pt.1" e "Famous".
         </p>
-        <a href="https://open.spotify.com/intl-pt/album/7D2NdGvBHIavgLhmcwhluK?si=N-1yvl95R76Qe1a8BcddMQ">
+        <a href="https://open.spotify.com/intl-pt/album/7gsWAHLeT0w7es6FofOXk1?si=-TIGmT8aRIi22qEEkOS_VQ">
           <img className="platform-icon" src="/spotify.png" alt="Spotify" />
         </a>
         <a href="https://music.apple.com/us/album/graduation/1440835365">
@@ -350,7 +425,7 @@ function App() {
         <p style={{ color: pColor }}>
           "ye" é o nono álbum de Kanye West, lançado em 2018. O álbum inclui faixas como "All Mine", "Ghost Town" e "Violent Crimes".
         </p>
-        <a href="https://open.spotify.com/intl-pt/album/7D2NdGvBHIavgLhmcwhluK?si=N-1yvl95R76Qe1a8BcddMQ">
+        <a href="https://open.spotify.com/intl-pt/album/2Ek1q2haOnxVqhvVKqMvJe?si=0dEGdVM0Q7KdlCEAbeXFXg">
           <img className="platform-icon" src="/spotify.png" alt="Spotify" />
         </a>
         <a href="https://music.apple.com/us/album/graduation/1440835365">
@@ -364,7 +439,7 @@ function App() {
         <p style={{ color: pColor }}>
           "KIDS SEE GHOSTS" é o décimo álbum de Kanye West, lançado em 2018. O álbum inclui faixas como "Fire", "Reborn" e "Kids See Ghosts".
         </p>
-        <a href="https://open.spotify.com/intl-pt/album/7D2NdGvBHIavgLhmcwhluK?si=N-1yvl95R76Qe1a8BcddMQ">
+        <a href="https://open.spotify.com/intl-pt/album/6pwuKxMUkNg673KETsXPUV?si=CiAtGv-5SVOQFfPrz_RrJg">
           <img className="platform-icon" src="/spotify.png" alt="Spotify" />
         </a>
         <a href="https://music.apple.com/us/album/graduation/1440835365">
@@ -378,7 +453,7 @@ function App() {
         <p style={{ color: pColor }}>
           "JESUS IS KING" é o décimo primeiro álbum de Kanye West, lançado em 2019. O álbum inclui faixas como "Follow God", "Closed On Sunday" e "God Is".
         </p>
-        <a href="https://open.spotify.com/intl-pt/album/7D2NdGvBHIavgLhmcwhluK?si=N-1yvl95R76Qe1a8BcddMQ">
+        <a href="https://open.spotify.com/intl-pt/album/0FgZKfoU2Br5sHOfvZKTI9?si=7Bvs4rLwR_O_iYtkUteKzg">
           <img className="platform-icon" src="/spotify.png" alt="Spotify" />
         </a>
         <a href="https://music.apple.com/us/album/graduation/1440835365">
@@ -419,7 +494,7 @@ function App() {
         <p style={{ color: pColor }}>
           "Donda" é o décimo segundo álbum de Kanye West, lançado em 2021. O álbum inclui faixas como "Jail", "Off The Grind", "Hurricane", "Praise God", e "Moon".
         </p>
-        <a href="https://open.spotify.com/intl-pt/album/7D2NdGvBHIavgLhmcwhluK?si=N-1yvl95R76Qe1a8BcddMQ">
+        <a href="https://open.spotify.com/intl-pt/album/5CnpZV3q5BcESefcB3WJmz?si=e9OW-YdtRISofjxBPrdUDw">
           <img className="platform-icon" src="/spotify.png" alt="Spotify" />
         </a>
         <a href="https://music.apple.com/us/album/graduation/1440835365">
@@ -434,7 +509,7 @@ function App() {
         <p style={{ color: pColor }}>
           "VULTURES 1" é o décimo terceiro álbum de Kanye West, lançado em 2024. O álbum inclui faixas como "KEYS TO MY LIFE", "BURN" e "CARNIVAL".
         </p>
-        <a href="https://open.spotify.com/intl-pt/album/7D2NdGvBHIavgLhmcwhluK?si=N-1yvl95R76Qe1a8BcddMQ">
+        <a href="https://open.spotify.com/intl-pt/album/0v9QGZDE2msGl8zAlXKP93?si=fX4GxvlfRpW9XIgChJunbg">
           <img className="platform-icon" src="/spotify.png" alt="Spotify" />
         </a>
         <a href="https://music.apple.com/us/album/graduation/1440835365">
@@ -449,7 +524,7 @@ function App() {
         <p style={{ color: pColor }}>
           "VULTURES 2" é o décimo quarto álbum de Kanye West, é a continuação do álbum anterior "VULTURES 1" lançado em 2024. O álbum inclui faixas como "FIELD TRIP", "FOREVER" e "530".
         </p>
-        <a href="https://open.spotify.com/intl-pt/album/7D2NdGvBHIavgLhmcwhluK?si=N-1yvl95R76Qe1a8BcddMQ">
+        <a href="https://open.spotify.com/intl-pt/album/5RV2TNyjylqWJNxQyHBTeJ?si=cbnj3uDESoau_aHk3IqJSg">
           <img className="platform-icon" src="/spotify.png" alt="Spotify" />
         </a>
         <a href="https://music.apple.com/us/album/graduation/1440835365">
